decac3a0da60f697927a7814d3a21481042386d5
Andreas <andreas@maanex.me>
2023-05-11 14:00:01 +0000

    move to monorepo

---
+++ apps/frontend/components/panels/Navigation.vue
@@ -0,0 +1,190 @@
+<template>
+  <div class="container">
+    <div class="pilotcontainer">
+      <img ref="pilot" class="pilot" src="~/assets/img/pilot.svg" alt="" draggable="false">
+      <div
+        class="handle direction"
+        userselect
+        :style="handleDirectionCss"
+        :data-grabbed="moveHandle === 'direction'"
+        @mousedown="moveHandle = 'direction'"
+      />
+      <div
+        class="handle accl"
+        userselect
+        :style="handleAcclCss"
+        :data-grabbed="moveHandle === 'accl'"
+        @mousedown="moveHandle = 'accl'"
+      />
+      <div
+        class="handle scan"
+        userselect
+        :style="handleScanCss"
+        :data-grabbed="moveHandle === 'scan'"
+        @mousedown="moveHandle = 'scan'"
+      />
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+const pilot = ref(null)
+const { x, y } = useMouse()
+const { pressed } = useMousePressed()
+
+const moveHandle = useState<null | 'direction' | 'accl' | 'scan'>(() => null)
+watch(pressed, (val) => { if (!val) moveHandle.value = null })
+
+const handleDirection = useState(() => 0)
+const handleDirectionCss = useState<any>(() => {})
+function handleMoveDirection() {
+  const bounds = (pilot.value! as Element)?.getBoundingClientRect()
+  const radius = bounds.height / 2
+  const midX = bounds.left + radius
+  const midY = bounds.top + radius
+
+  const deltaX = x.value - midX
+  const deltaY = y.value - midY
+  const rad = Math.atan2(deltaY, deltaX)
+  const radDeg = ((rad / Math.PI * 180) + 450) % 360
+  handleDirection.value = radDeg
+}
+
+const handleAccl = useState(() => 0)
+const handleAcclCss = useState<any>(() => {})
+function handleMoveAccl() {
+  const bounds = (pilot.value! as Element)?.getBoundingClientRect()
+  const radius = bounds.height * 0.4
+  const midX = bounds.right - radius
+  const midY = bounds.top + radius
+
+  const deltaX = x.value - midX
+  const deltaY = y.value - midY
+  const rad = Math.atan2(deltaY, deltaX)
+  const outVal = (rad < 0)
+    ? (1 - (-rad / Math.PI))
+    : (rad < Math.PI/2 ? 1 : 0)
+  handleAccl.value = outVal
+}
+
+const handleScan = useState(() => 0)
+const handleScanCss = useState<any>(() => {})
+function handleMoveScan() {
+  const bounds = (pilot.value! as Element)?.getBoundingClientRect()
+  const radius = bounds.height * 0.4
+  const midX = bounds.right - radius
+  const midY = bounds.bottom - radius
+
+  const deltaX = x.value - midX
+  const deltaY = y.value - midY
+  const rad = Math.atan2(deltaY, deltaX)
+  const outVal = (rad > 0)
+    ? (1 - (rad / Math.PI))
+    : (rad < -Math.PI/2 ? 0 : 1)
+
+  handleScan.value = outVal
+}
+
+function handleMove() {
+  switch (moveHandle.value) {
+    case null: return
+    case 'direction': return handleMoveDirection()
+    case 'accl': return handleMoveAccl()
+    case 'scan': return handleMoveScan()
+  }
+}
+watch(x, handleMove)
+watch(y, handleMove)
+
+function updateDirection() {
+  const bounds = (pilot.value! as Element)?.getBoundingClientRect()
+  const radius = bounds.height / 2
+
+  const dirX = Math.cos((handleDirection.value - 90) / 180 * Math.PI) * radius * 0.96 + radius
+  const dirY = Math.sin((handleDirection.value - 90) / 180 * Math.PI) * radius * 0.96 + radius
+
+  handleDirectionCss.value = { top: `${dirY}px`, left: `${dirX}px` }
+}
+
+function updateAccl() {
+  const bounds = (pilot.value! as Element)?.getBoundingClientRect()
+  const radius = bounds.height * 0.4
+  const midX = bounds.width - radius
+  const midY = radius
+  const dirX = Math.cos((handleAccl.value - 1) * Math.PI) * radius * 0.96 + midX
+  const dirY = Math.sin((handleAccl.value - 1) * Math.PI) * radius * 0.96 + midY
+
+  handleAcclCss.value = { top: `${dirY}px`, left: `${dirX}px` }
+}
+
+function updateScan() {
+  const bounds = (pilot.value! as Element)?.getBoundingClientRect()
+  const radius = bounds.height * 0.4
+  const midX = bounds.width - radius
+  const midY = bounds.height - radius
+  const dirX = Math.cos((handleScan.value - 1) * Math.PI) * radius * 0.96 + midX
+  const dirY = midY - Math.sin((handleScan.value - 1) * Math.PI) * radius * 0.96
+
+  handleScanCss.value = { top: `${dirY}px`, left: `${dirX}px` }
+}
+
+function updateAll() {
+  updateDirection()
+  updateAccl()
+  updateScan()
+}
+
+onMounted(updateAll)
+onUpdated(updateAll)
+useResizeObserver(pilot, updateAll)
+watch(handleDirection, updateDirection)
+watch(handleAccl, updateAccl)
+watch(handleScan, updateScan)
+</script>
+
+<style scoped lang="scss">
+.container {
+  background-color: $color-beige;
+  background-image: url('~/assets/img/noise-10p.png');
+  animation: bg-jitter 1s steps(1) forwards infinite;
+  padding: $gap;
+  display: flex;
+  flex-direction: column;
+
+  .pilotcontainer {
+    position: relative;
+  }
+
+  .pilot {
+    width: 100%;
+    aspect-ratio: 100/50;
+    opacity: .7;
+  }
+
+  .handle {
+    position: absolute;
+    width: 1vw;
+    height: 1vw;
+    border: .4vw solid mix($color-beige, #000000, 30%);
+    background-color: $color-beige;
+    border-radius: 999pt;
+    transform: translate(-50%, -50%);
+    cursor: grab;
+
+    &:hover, &[data-grabbed=true] {
+      border-color: #000000;
+    }
+
+    &::after {
+      content: '';
+      width: 300%;
+      height: 300%;
+      top: -100%;
+      left: -100%;
+      display: block;
+      position: absolute;
+      border-radius: 999pt;
+    }
+  }
+}
+</style>
---
+++ apps/frontend/components/panels/Radar.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="container">
+    
+  </div>
+</template>
+
+<style scoped lang="scss">
+.container {
+  background-color: #444444;
+  background-image: url('~/assets/img/noise-10p.png');
+  animation: bg-jitter 1s steps(1) forwards infinite;
+}
+</style>
