d6933499c6cc2264a7e44e5c4f2a9dabd536e050
Andreas <andreas@maanex.me>
2023-05-24 11:09:31 +0000

    yea

--- apps/frontend/composables/documents.ts
+++ apps/frontend/composables/documents.ts
@@ -17,18 +17,33 @@ export const useDocumentData = () => useState<Document[]>('document-data', () =>
     text: 'Hello. Welcome. You can find your ship\'s position data in the bottom left corner. The panel to the right of your position is your control panel. You can steer your ship there. Grab the handles to set a course and accelerate.'
   },
   {
-    key: 'howtogame',
-    title: 'How to game',
-    text: 'Start gaming.'
+    key: 'readwrite',
+    title: 'Intro: Messages',
+    text: 'Great, you know how to move now. See those green gems around you? You may need to zoom out a bit. Those are messages left by other people. You can hover them with your mouse to see what they wrote. You can also leave your own messages using the third panel at the bottom. Just be aware that leaving messages costs resources. More on them later. The longer the message or the more other messages nearby, the more expensive it is to write something. Go try it out!'
   },
   {
-    key: 'howtoungame',
-    title: 'How to un-game',
-    text: 'Do not ungame. Serious warning.'
+    key: 'mining',
+    title: 'Intro: Getting resources',
+    text: 'TODO'
   },
   {
-    key: 'bananas',
-    title: 'What are bananas',
-    text: 'Bananas are long curved yellow objects commonly used to feed monkeys and monkey-like creatures such as bongumas. Bananas are not to be messed with as bananas have quite the tactical advantage with areal attacks ranging up to 15km in their prime age.'
-  }
+    key: 'otherperson',
+    title: 'Other Explorers',
+    text: 'Did you see this? Another ship. An unknown explorer, just like you. Roaming around. Exploring. Say hi.'
+  },
+  {
+    key: 'ring1',
+    title: 'Ring 1',
+    text: 'You entered ring 1. Be careful. Something in the center is emitting strong radiation. Obfuscating your radar. Obfuscating your other sensors. Don\'t get lost. We need you.'
+  },
+  {
+    key: 'ring3',
+    title: 'Ring 3',
+    text: 'You entered ring 3. It\'s quiet here. Peaceful. Empty.'
+  },
+  {
+    key: 'outerring',
+    title: 'The unknown',
+    text: 'You have left ring 3. The world beyond is not documented. Be careful.'
+  },
 ])
